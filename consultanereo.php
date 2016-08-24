75aa9432a97ad3125304ce551d3ab17d8759f855
Fernando Celi <fernando_dfcv@hotmail.com>
2016-08-24 16:19:42 -0500

    Cambio en conexión a BD

--- consultanereo.php
+++ consultanereo.php
@@ -1,22 +1,22 @@
 <?php
-require("config.inc.php");
 require("conexion_servidor_bd.php");
-session_start();
-$cod=$_SESSION['codigo'];
 
-$consultar_registros=" SELECT EST_COD,EST_NRO_IDEN,EST_NOMBRE,EST_CRA_COD CODIGOCRA,CRA_NOMBRE CRANOM,EST_ACUERDO,EST_PEN_NRO,CASE WHEN EST_IND_CREO='N' THEN 'HORAS' WHEN EST_IND_CREO='S' THEN 'CRÉDITOS' ELSE 'N/A' END TIPO_PLAN,EOT_EMAIL_INS,EOT_EMAIL,EOT_TIPOSANGRE,EOT_RH
-FROM accest
-INNER JOIN acestotr ON EST_COD=EOT_COD
-INNER JOIN accra ON CRA_COD=EST_CRA_COD
+$consultar_registros="SELECT EST_COD,EST_NRO_IDEN,EST_NOMBRE,EST_CRA_COD||'-'||CRA_NOMBRE,EST_ACUERDO,EST_PEN_NRO,CASE WHEN EST_IND_CRED='N' THEN 'HORAS' WHEN EST_IND_CRED='S' THEN 'CRÉDITOS' ELSE 'N/A' END TIPO_PLAN,EOT_EMAIL_INS,EOT_EMAIL,EOT_TIPOSANGRE,EOT_RH
+FROM ACEST
+INNER JOIN ACESTOTR ON EST_COD=EOT_COD
+INNER JOIN ACCRA ON CRA_COD=EST_CRA_COD
 WHERE EST_COD IN (20122078098)";
 
+//echo $consultar_registros;
+$cadenaParser = OCIParse($conectado,$consultar_registros);
 
+$busqueda=OCIExecute($cadenaParser);
 
-$registros=mysql_db_query("l6000018_nereo",$consultar_registros,$conectado) or die(" No se puede ejecutar la consulta:". mysql_error());
-
-while ($tabla=mysql_fetch_array($registros)){
-	$datos[]=$tabla;
-	}
+if ($busqueda) {
+	while ($tabla=oci_fetch_array($cadenaParser, OCI_BOTH)){
+                $datos[]=$tabla;
+        }
+}
 
 echo json_encode($datos);
 
--- consultar_notasparciales.php
+++ consultar_notasparciales.php
@@ -1,14 +1,13 @@
 <?php
 require("conexion_servidor_bd.php");
-session_start();
-$cod=$_SESSION['codigo'];
-$consultar_registros=" SELECT DISTINCT
+
+$consultar_registros = "SELECT DISTINCT
   INS_ASI_COD COD_ESPACIO,
   ASI_NOMBRE NOMBRE_ESPACIO,
-  CUR_CRA_COD CODIGOCRA,CUR_GRUPO GRUPO,
+  CUR_CRA_COD||'-'||CUR_GRUPO GRUPO,
   INS_EST_COD COD_EST,
   EST_NOMBRE NOMBRE_ESTUDIANTE,
-  ELT (NOB_NOMBRE,'SIN OBSERVACION','',NOB_NOMBRE) OBS,
+  DECODE (NOB_NOMBRE,'SIN OBSERVACION','',NOB_NOMBRE) OBS,
   INS_ANO ANO,
   INS_PER PERIODO,
   CUR_PAR1 PPAR,
@@ -32,26 +31,31 @@ $consultar_registros=" SELECT DISTINCT
   INS_NOTA_ACU NOTA_ACUM,
   INS_NOTA NOTA_DEF,  
   INS_TOT_FALLAS FALLAS,
-  DOC_NOMBRE DOCNOMBRE ,DOC_APELLIDO DOCENTEAPE
-  FROM acins
-INNER JOIN accursos ON CUR_ID=INS_GR
-INNER JOIN achorarios ON HOR_ID_CURSO=CUR_ID
-INNER JOIN accest ON EST_COD=INS_EST_COD
-INNER JOIN acasi ON ASI_COD=INS_ASI_COD
-INNER JOIN acasperi ON APE_ANO=INS_ANO AND APE_PER=INS_PER
-LEFT OUTER JOIN acnotobs ON NOB_COD=INS_OBS
-LEFT OUTER JOIN accargas ON CAR_HOR_ID=HOR_ID
-LEFT OUTER JOIN acdocente ON DOC_NRO_IDEN=CAR_DOC_NRO
+  DOC_NOMBRE||' '||DOC_APELLIDO DOCENTE
+  FROM ACINS
+INNER JOIN ACCURSOS ON CUR_ID=INS_GR
+INNER JOIN ACHORARIOS ON HOR_ID_CURSO=CUR_ID
+INNER JOIN ACEST ON EST_COD=INS_EST_COD
+INNER JOIN ACASI ON ASI_COD=INS_ASI_COD
+INNER JOIN ACASPERI ON APE_ANO=INS_ANO AND APE_PER=INS_PER
+LEFT OUTER JOIN ACNOTOBS ON NOB_COD=INS_OBS
+LEFT OUTER JOIN ACCARGAS ON CAR_HOR_ID=HOR_ID
+LEFT OUTER JOIN ACDOCENTE ON DOC_NRO_IDEN=CAR_DOC_NRO
 WHERE HOR_ESTADO='A'
 AND APE_ESTADO='A'
-AND EST_COD IN (20122078098)
-ORDER BY INS_ANO,INS_PER,INS_EST_COD,INS_ASI_COD,CUR_CRA_COD||'-'||CUR_GRUPO;";
-$registros=mysql_db_query("l6000018_nereo",$consultar_registros,$conectado) or die(" No se puede ejecutar la consulta:". mysql_error());
+AND EST_COD IN (20051085002)
+ORDER BY INS_ANO,INS_PER,INS_EST_COD,INS_ASI_COD,CUR_CRA_COD||'-'||CUR_GRUPO";
 
-while ($tabla=mysql_fetch_array($registros)){
-	$datos[]=$tabla;
-	}
+//echo $consultar_registros;
+$cadenaParser = OCIParse($conectado,$consultar_registros);
 
+$busqueda=OCIExecute($cadenaParser);
+
+if ($busqueda) {
+        while ($tabla=oci_fetch_array($cadenaParser, OCI_BOTH)){
+                $datos[]=$tabla;
+        }
+}
 echo json_encode($datos);
 
 ?>
