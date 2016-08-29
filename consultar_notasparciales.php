c4ce13d2066640620cf92c0011c90affb5056de9
ricki2099 <ricki2099@hotmail.com>
2016-08-29 11:47:04 -0500

    Update consultar_notasparciales.php

--- consultar_notasparciales.php
+++ consultar_notasparciales.php
@@ -1,6 +1,18 @@
 <?php
 require("conexion_servidor_bd.php");
 $codigo = isset($_GET['codEstudiante'])?$_GET['codEstudiante']:'';
+if( isset($_GET['codEstudiante']) ) {
+        $jsondata['success'] = true;
+        $jsondata['message'] = 'Valor recibido es correcto.';
+        echo ($jsondata);
+
+    } else {
+
+        $jsondata['success'] = false;
+        $jsondata['message'] = 'Valor recibido no es correcto.';
+        echo ($jsondata);
+
+    }
 if ($codigo != '') {
 $consultar_registros = "SELECT DISTINCT
   INS_ASI_COD COD_ESPACIO,
