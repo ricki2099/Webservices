afb20894c64ee1bc3273f608af2746b02a366fe7
ricki2099 <ricki2099@hotmail.com>
2016-09-02 10:52:32 -0500

    Update noticia1.php

--- noticia1.php
+++ noticia1.php
@@ -1,6 +1,19 @@
 <?php
 require("conexion_servidor_bd.php");
-$consultar_registros="SELECT * FROM noticia ";
+$consultar_registros="SELECT
+nombre,
+descripcion,
+enlace,
+tipo,
+anio,
+periodo,
+fecha_radicacion,
+usr_remitente,
+imagen
+FROM general.noticia
+WHERE estado=1
+AND now()::date BETWEEN fecha_inicio AND fecha_fin
+ORDER BY fecha_radicacion DESC";
 //oci_parse('SET CHARACTER SET utf8');
 // se cambia esta linea
 $registros=OCIParse($conectado,$consultar_registros);
