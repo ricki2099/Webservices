013d5bf50d55d1327651aada396bbf93c72d4797
ricki2099 <ricki2099@hotmail.com>
2016-09-02 10:29:42 -0500

    Update noticia1.php

--- noticia1.php
+++ noticia1.php
@@ -1,16 +1,14 @@
 <?php
 require("conexion_servidor_bd.php");
-$consultar_registros="SELECT * FROM noticia;";
-mysql_query('SET CHARACTER SET utf8');
-$registros=mysql_db_query("l6000018_nereo",$consultar_registros,$conectado);
+$consultar_registros="SELECT * FROM noticia ";
+oci_parse('SET CHARACTER SET utf8');
+// se cambia esta linea
+$registros=OCIParse($conectado,$consultar_registros);
 //$totalregistros=mysql_num_rows($registros);
-while ($tabla=mysql_fetch_array($registros)){
+//se cambia linea
+while ($tabla=oci_fetch_array($registros, OCI_BOTH)){
 	$datos[]=$tabla;
 	}
-
 //header("Content-Type: image/gif");
 echo json_encode($datos);
-
-
-
 ?>
