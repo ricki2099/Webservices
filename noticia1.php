72d7a0f1957b8bc7cb8398975a2ff448f8ac59f1
ricki2099 <ricki2099@hotmail.com>
2016-09-02 11:06:56 -0500

    Update noticia1.php

--- noticia1.php
+++ noticia1.php
@@ -1,5 +1,7 @@
 <?php
 require("conexion_servidor_bd.php");
+
+
 $consultar_registros="SELECT
 nombre,
 descripcion,
@@ -14,14 +16,29 @@ FROM general.noticia
 WHERE estado=1
 AND now()::date BETWEEN fecha_inicio AND fecha_fin
 ORDER BY fecha_radicacion DESC";
+
 //oci_parse('SET CHARACTER SET utf8');
+
 // se cambia esta linea
+
 $registros=OCIParse($conectado,$consultar_registros);
+
 //$totalregistros=mysql_num_rows($registros);
+
 //se cambia linea
+$busqueda=OCIExecute($registros);
+	if ($busqueda) {
+
 while ($tabla=oci_fetch_array($registros, OCI_BOTH)){
+
 	$datos[]=$tabla;
+
 	}
+}else {
+	$datos = array('No hay noticias');
+}
 //header("Content-Type: image/gif");
+
 echo json_encode($datos);
+
 ?>
