5d4f2054a45b136f11cb84e4ad3168decbfa17d6
ricki2099 <ricki2099@hotmail.com>
2016-09-05 13:55:16 -0500

    Update ejemplo2.php

--- ejemplo2.php
+++ ejemplo2.php
@@ -1,7 +1,7 @@
 <?php
 include("simple_html_dom.php") ;
 
-$html = file_get_html("http://jcgrafficdesigns.net/Nereo/ejemplo1.php");
+$html = file_get_html("10.20.0.149/urano/index.php?data=sj7574MlJOsg4LjjeAOJP5CBi1dRh84M-gX_Z-i_0Ol7TIQjdwZpa36rW8uzbT71_GMpcRAta2MSZGt9OmeQN3O0c9_4iv7A6ODAQz8nzk3-L-wp9KXARJdYvqggsPUb");
 
 foreach($html->find('json') as $noscript)
 {
